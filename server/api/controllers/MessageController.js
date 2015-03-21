e1260d353ab90cbb019bd37554ee68396bca2657
Lamin Sanneh <lamin.evra@gmail.com>
2015-03-21 10:51:27 +0000

    initial commit

---
+++ server/api/controllers/MessageController.js
@@ -0,0 +1,12 @@
+/**
+ * MessageController
+ *
+ * @description :: Server-side logic for managing Messages
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+};
+
