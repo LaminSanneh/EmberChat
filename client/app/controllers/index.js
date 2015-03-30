db5bfab90b983ba80863a0a207810b075be1092a
Lamin Sanneh <lamin.evra@gmail.com>
2015-03-30 18:48:24 +0000

    Add ability to delete message

--- client/app/controllers/index.js
+++ client/app/controllers/index.js
@@ -3,10 +3,15 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   actions: {
     messageSubmitted: function(messageBody){
-      console.log(this.get('messageBody'));
+
       this.store.createRecord('message', {
         body: messageBody
       }).save();
+    },
+
+    deleteMessage: function(message) {
+
+      message.destroyRecord();
     }
   }
 });
