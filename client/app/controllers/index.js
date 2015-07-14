48ea68d13c1aeb76101e9af84b0ef3c24ce0dc07
Lamin Sanneh <lamin.evra@gmail.com>
2015-07-14 05:12:12 +0000

    removed unneeded code

--- client/app/controllers/index.js
+++ client/app/controllers/index.js
@@ -1,6 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  needs: ['application'],
-  currentUser: Ember.computed.alias('controllers.application.currentUser')
 });
--- client/app/routes/index.js
+++ client/app/routes/index.js
@@ -2,13 +2,6 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
-  model: function(){
-    return this.store.find("message");
-  },
-  setupController: function (controller, model) {
-    this._super(controller, model);
-    controller.set('loggedInUsers', this.store.find('user'));
-  },
   redirect: function () {
     return this.transitionTo('chatSessions');
   }
